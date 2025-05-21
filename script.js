0c08c1a22fb919f8f1fab54478c29c86b4d2e08b
Adedeji John Abidemi <adedejijohn099@gmail.com>
2025-05-20 19:34:19 -0500

    add a functionality to the sidebar links so it can be close when click

--- script.js
+++ script.js
@@ -3,6 +3,12 @@ const showsideBar = document.querySelector(".showsidebar");
 const cancelBtn = document.querySelector(".cancelbtn");
 const sidebar = document.querySelector(".sidebar");
 
+document.querySelectorAll(".sidebar li").forEach((links) => {
+  links.addEventListener("click", () => {
+    sidebar.style.display = "none";
+  });
+});
+
 showsideBar.addEventListener("click", () => {
   sidebar.style.display = "flex";
 });
